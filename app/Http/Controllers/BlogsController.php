0bb6d43eda1d15945bf741926832ee980aa1f526
hguruman <hguruman@gmail.com>
2021-04-12 15:56:36 +0400

    Form submission 2

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -18,12 +18,12 @@ class BlogsController extends Controller
     }
 
     public function store(Request $request){
-        // $input = $request->all();
-        // $blog = Blog::create($input);
-        $blog = new Blog();
-        $blog->title = $request->title;
-        $blog->body = $request->body;
-        $blog->save();
+        $input = $request->all();
+        $blog = Blog::create($input);
+        // $blog = new Blog();
+        // $blog->title = $request->title;
+        // $blog->body = $request->body;
+        // $blog->save();
         return redirect('/blogs');
     }
 }
