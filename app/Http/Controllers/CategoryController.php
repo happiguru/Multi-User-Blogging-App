58c555f418613a7782124ee2a5d31e353ec3f67c
hguruman <hguruman@gmail.com>
2021-04-14 16:17:53 +0400

    Add index function and index view for the categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,6 +15,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $categories = Category::latest()->get();
+        return view('categories.index', ['categories', $categories]);
     }
 
     /**
